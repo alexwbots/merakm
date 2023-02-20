144d846219a9c874443cdc534706af88321c08b8
Alexander López <lopez.manrique.alexander@gmail.com>
2023-02-20 14:55:27 -0500

    Update odroid_setup.sh

--- odroid_setup.sh
+++ odroid_setup.sh
@@ -1,19 +1,19 @@
 echo "Uninstall software not need for the robot."
-sudo apt-get remove --purge libreoffice*
+sudo apt-get remove -y --purge libreoffice*
 sudo apt-get update
-sudo apt-get upgrade
+sudo apt-get upgrade -y
 
 echo "Install ROS NOETIC."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install curl
+sudo apt install -y curl
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
-sudo apt install ros-noetic-desktop
+sudo apt install -y ros-noetic-desktop
 echo "source /opt/ros/noetic/setup.bash" >> ~/.bashrc
 source ~/.bashrc
 
 echo "Install ROS packages."
-sudo apt-get install ros-noetic-joy ros-noetic-teleop-twist-joy ros-noetic-teleop-twist-keyboard ros-noetic-laser-proc \
+sudo apt-get install -y ros-noetic-joy ros-noetic-teleop-twist-joy ros-noetic-teleop-twist-keyboard ros-noetic-laser-proc \
   ros-noetic-rgbd-launch ros-noetic-rosserial-arduino ros-noetic-rosserial-python ros-noetic-rosserial-client \
   ros-noetic-rosserial-msgs ros-noetic-amcl ros-noetic-map-server ros-noetic-move-base ros-noetic-urdf ros-noetic-xacro \
   ros-noetic-compressed-image-transport ros-noetic-rqt* ros-noetic-rviz ros-noetic-gmapping ros-noetic-navigation \
@@ -27,6 +27,6 @@ catkin_make
 cd
 
 echo "Install additional packages"
-sudo apt-get install openssh-client
-sudo apt install python3-pip
+sudo apt-get install -y openssh-client
+sudo apt install -y python3-pip
 pip install odroid-wiringpi
