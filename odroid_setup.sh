6e9ed186734b15403da99bf51728772e74c08b30
Alexander López <lopez.manrique.alexander@gmail.com>
2023-02-21 23:55:16 -0500

    Update odroid_setup.sh

--- odroid_setup.sh
+++ odroid_setup.sh
@@ -5,7 +5,7 @@ sudo apt-get upgrade -y
 
 echo "Install ROS NOETIC."
 sudo sh -c 'echo "deb http://packages.ros.org/ros/ubuntu $(lsb_release -sc) main" > /etc/apt/sources.list.d/ros-latest.list'
-sudo apt install -y curl
+sudo apt install -y curl git
 curl -s https://raw.githubusercontent.com/ros/rosdistro/master/ros.asc | sudo apt-key add -
 sudo apt update
 sudo apt install -y ros-noetic-desktop
